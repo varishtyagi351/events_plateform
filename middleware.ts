1831966330229a0113ab0ba601a738e7962b654a
VarishTyagi <varishtyagi351@gmail.com>
2024-07-03 16:14:41 +0530

    again finish the app

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/', '/api/uploadthing'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/', '/api/uploadthing(.*)', '/api/webhooks(.*)'])
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) auth().protect();
